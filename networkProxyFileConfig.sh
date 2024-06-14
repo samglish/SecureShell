0ba4115c00f3d8bb82c862bea61ac166e1713037
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2024-06-15 00:43:52 +0100

    update

---
+++ networkProxyFileConfig.sh
@@ -0,0 +1,9 @@
+echo "install nano"
+sudo apt install nano
+echo "install proxychains"
+sudo apt install proxychains-ng
+echo "configure proxychains"
+sudo sed -i 's/quiet/quiet dns/g' /etc/proxychains.conf
+sudo sed -i 's/socks5/socks5 127.0.0.1 9050/g' /etc/proxychains.conf
+
+sudo nano /etc/proxychains.conf 
